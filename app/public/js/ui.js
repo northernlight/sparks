3a97eb02ca9931177d1c393c0d52a004a64cbadb
Kanibal <kanibalclonk@freenet.de>
2015-11-23 15:34:29 -0600

    added graph visualization for debugging purposes

--- app/public/js/ui.js
+++ app/public/js/ui.js
@@ -30,6 +30,69 @@ var app = null;
     window.location.assign("/");
   };
 
+  app.onButtonDeveloperClick = function() {
+
+    // knock on the network
+    app.requestNetworkMesh();
+    // and listen to the sound
+    document.getElementById('developerDialog').open();
+    var myNode = document.getElementById("graph-container");
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+    }
+    var s = new sigma({
+      renderer: {
+        container: document.getElementById('graph-container'),
+        type: 'canvas'
+      },
+      settings: {
+        doubleClickEnabled: false,
+        minEdgeSize: 0.5,
+        maxEdgeSize: 4,
+        enableEdgeHovering: true,
+        edgeHoverColor: 'edge',
+        defaultEdgeHoverColor: '#000',
+        edgeHoverSizeRatio: 1,
+        edgeHoverExtremities: true,
+      }
+    });
+    s.graph.addNode({
+      id: app.me.id,
+      label: 'You: ' + app.me.name,
+      x: Math.random() * 600,
+      y: Math.random() * 400,
+      size: 26,
+      color: '#333'
+    });
+    _.forEach(_.values(app.users), function(user) {
+      s.graph.addNode({
+        id: user.id,
+        label: 'Peer: ' + user.name,
+        x: Math.random() * 600,
+        y: Math.random() * 400,
+        size: 16,
+        color: '#666'
+      });
+    });
+    app.onInfoReceive = function(msg) {
+      _.forOwn(msg.data, function(value, key) {
+        _.forEach(value, function(val) {
+          s.graph.addEdge({
+            id: 'e' + key + val,
+            source: key,
+            target: val,
+            size: Math.random(),
+            type: 'curve',
+            color: '#ccc',
+            hover_color: '#000'
+          });
+        });
+      });
+      s.refresh();
+    };
+    s.refresh();
+  };
+
   app.showMessage = function(msg) {
     var toast = document.querySelector('#toast');
     toast.set('text', msg);
@@ -100,6 +163,7 @@ var app = null;
             }
             break;
           case 'Set':
+            app.onInfoReceive(msg);
             console.warn("MsgInfo: set() not yet implemented")
             break;
           default:
@@ -111,7 +175,7 @@ var app = null;
     }
   }
 
-  app.getMesh = function() {
+  app.requestNetworkMesh = function() {
     app.broadcast(JSON.stringify({
       type: "MsgFlood",
       ttl: 8,
